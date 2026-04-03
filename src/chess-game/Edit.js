9f1fe75d893e02a8d272fa30de12a0f17fd36ba6
Julien Verneaut <jverneaut@gmail.com>
2026-04-03 19:07:33 +0200

    Add chess.js moves validation

--- src/chess-game/Edit.js
+++ src/chess-game/Edit.js
@@ -1,10 +1,41 @@
+import { useRef, useState } from "react";
+
 import { useBlockProps } from "@wordpress/block-editor";
 import { Chessboard } from "./react-chessboard-compat";
+import { Chess } from "chess.js";
 
 export default () => {
+	const chessGameRef = useRef(new Chess());
+	const chessGame = chessGameRef.current;
+
+	const [position, setPosition] = useState(chessGame.fen());
+
+	const onPieceDrop = ({ sourceSquare, targetSquare }) => {
+		if (!targetSquare) return false;
+
+		try {
+			chessGame.move({
+				from: sourceSquare,
+				to: targetSquare,
+				promotion: "q",
+			});
+
+			setPosition(chessGame.fen());
+
+			return true;
+		} catch {
+			return false;
+		}
+	};
+
 	return (
 		<div {...useBlockProps()}>
-			<Chessboard />
+			<Chessboard
+				options={{
+					position,
+					onPieceDrop,
+				}}
+			/>
 		</div>
 	);
 };
