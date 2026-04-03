4f491ebf633653cb205eeda0b61a344507ff78ff
Julien Verneaut <jverneaut@gmail.com>
2026-04-03 17:35:59 +0200

    Scaffold block plugin using @wordpress/create-block

---
+++ build/gutenberg-chess/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd453e00203c4f85efc9b');
---
+++ build/gutenberg-chess/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '253016136260f41af679');
