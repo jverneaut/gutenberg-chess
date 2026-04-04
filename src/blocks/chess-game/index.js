666da30e1cddb43abf757a2af47a4c9142670b92
Julien Verneaut <jverneaut@gmail.com>
2026-04-04 12:50:16 +0200

    Remove polling frequency override

--- src/blocks/chess-game/index.js
+++ src/blocks/chess-game/index.js
@@ -1,6 +1,5 @@
 import { InnerBlocks } from "@wordpress/block-editor";
 import { registerBlockType } from "@wordpress/blocks";
-import { addFilter } from "@wordpress/hooks";
 
 import metadata from "./block.json";
 import Edit from "./edit";
@@ -10,9 +9,3 @@ registerBlockType(metadata, {
 	edit: Edit,
 	save: () => <InnerBlocks.Content />,
 });
-
-addFilter(
-	"sync.pollingManager.pollingIntervalWithCollaborators",
-	"gutenberg-chess/polling",
-	() => 200,
-);
