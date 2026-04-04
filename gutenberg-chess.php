28d2ef748bea1bd7e3ee091babbc971f8b2c2f02
Julien Verneaut <jverneaut@gmail.com>
2026-04-04 03:19:05 +0200

    Version 0.1.3

--- gutenberg-chess.php
+++ gutenberg-chess.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       Gutenberg Chess
  * Description:       Real-time collaborative chess blocks for WordPress.
- * Version:           0.1.2
+ * Version:           0.1.3
  * Requires at least: 7.0
  * Requires PHP:      7.4
  * Author:            Julien Verneaut
