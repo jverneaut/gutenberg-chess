a27949d9ab97558a486f72a69e0188fde9bfb81d
Julien Verneaut <jverneaut@gmail.com>
2026-04-07 11:22:48 +0200

    Remove useless setAttributes call

--- src/blocks/chess-player-status/edit.js
+++ src/blocks/chess-player-status/edit.js
@@ -1,11 +1,10 @@
 import { useBlockProps } from "@wordpress/block-editor";
-import { useEffect } from "react";
 
 import { useMovesFromContext } from "../../hooks/useMovesFromContext";
 import { useResolvedPlayer } from "../../hooks/useResolvedPlayer";
 import { getPlayerStatusText } from "../../utils/player-status";
 
-const Edit = ({ attributes, context, setAttributes }) => {
+const Edit = ({ attributes, context }) => {
 	const { playerSide } = useResolvedPlayer({
 		context,
 		attributePlayerSide: attributes.playerSide,
@@ -13,14 +12,6 @@ const Edit = ({ attributes, context, setAttributes }) => {
 	const moves = useMovesFromContext(context);
 	const statusText = getPlayerStatusText(moves, playerSide);
 	const blockProps = useBlockProps();
-
-	useEffect(() => {
-		if (attributes.statusText === statusText) {
-			return;
-		}
-
-		setAttributes({ statusText });
-	}, [attributes.statusText, setAttributes, statusText]);
 
 	if (!statusText) {
 		return null;
